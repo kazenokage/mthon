aff9f5fbc47496b2580b01681fcd8211d710de80
Tommi Nikkanen <tommi.nikkanen@cs.helsinki.fi>
2015-10-01 19:41:50 +0300

    Server front updates

---
+++ front/assets/js/algo.js
@@ -0,0 +1,26 @@
+// Mthon 2015
+// Algorithm template
+// www.solinor.fi
+
+var MTHONALGO = MTHONALGO || {};
+
+// required base function for finding the path in <dataset>
+MTHONALGO.solveStellarRoute = function(dataset) {
+    var solution = {
+      // path consists of id:s of the stars in the path, in linear order start->end
+      // example: [23,654,234,2,64]
+      path: [],
+      // connections consits of individual connections between stars, identified by pairs of star ids
+      // example: [[1,56],[38,62]]
+      connections: [],
+      // length of the path
+      length: 0
+    }
+
+    // do your magic here
+
+    // required return
+    return solution;
+}
+
+// add your support functions in here
