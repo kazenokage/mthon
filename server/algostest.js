27a37f0890fb00ea1e633ed08d749128a76dba44
Jerry Mesimäki <jerry.mesimaki@solinor.com>
2015-10-02 19:30:14 +0300

    new algo, graph

---
+++ server/algostest.js
@@ -0,0 +1,103 @@
+function algorithm(dataset) {
+
+  function getStar(id, stars) {
+    var ret = stars.filter((s) => s._id === id);
+    return ret.length > 0 ? ret[0] : undefined;
+  }
+
+  function dist(x1,y1,z1,x2,y2,z2) {
+    return Math.sqrt( Math.pow(x2-x1,2) + Math.pow(y2-y1, 2) + Math.pow(z2-z1,2));
+  }
+
+  function distStars(s1, s2) {
+    return dist(s1.position.x, s1.position.y, s1.position.z, s2.position.x, s2.position.y, s2.position.z);
+  }
+
+  function findPathBetweenFinal(dataset) {
+    var graph = dataset.stars;
+    var source = dataset.stars[0];
+    var target = getStar(dataset.endPoint, graph);
+    return findPathBetween(graph, source, target);
+  }
+
+  function findPathBetween(graph, source, target) {
+    var solved = calcDistDijkstra(graph, source);
+    return getPath(target);
+  }
+
+  function constructNeighbors(graph) {
+    graph.forEach((star) => {
+      star.nghbrs = graph
+        .filter((pn) => distStars(star, pn) <= 30 && pn !== star)
+        .map((n) => n._id);
+    });
+    return graph;
+  }
+
+  function calcDistDijkstra(graph, source) {
+    constructNeighbors(graph);
+
+    function relax(u, v) {
+      if (v.dist > u.dist + distStars(u, v)) {
+        v.dist = u.dist + distStars(u, v);
+        v.par = u;
+      }
+    }
+
+    function getCheapest(q) {
+      return q.reduce((prev, cur) => prev.dist > cur.dist ? cur : prev, q[0]);
+    }
+
+    source.dist = 0;
+    var q = [];
+    var s = [];
+
+    graph.forEach(function(v) {
+      if (v !== source) {
+        v.dist = Number.MAX_VALUE;
+        v.par = undefined;
+      }
+      q.push(v);
+    });
+
+    while (q.length > 0) {
+      var cur = getCheapest(q);
+      q = q.filter(function(n) {
+        return n !== cur;
+      });
+      s.push(cur);
+      cur.nghbrs.forEach(function(n) {
+        relax(cur, getStar(n, graph));
+      });
+    }
+    return s;
+  }
+
+  function getPath(end) {
+    var ret = [];
+    while (end.par) {
+      ret.push(end._id);
+      end = end.par;
+    }
+    ret.push(end._id);
+    return ret.reverse();
+  }
+
+  function findFurthest(graph) {
+    var completedGraph = calcDistDijkstra(graph, graph[0]);
+    var furthest = completedGraph.reduce(function(prev, cur, idx, arr) {
+      if (cur.dist > prev.dist && cur.dist !== Number.MAX_VALUE) {
+        return cur;
+      } else {
+        return prev;
+      }
+    }, completedGraph[0]);
+    return furthest;
+  }
+
+  return findPathBetweenFinal(dataset);
+
+}
+module.exports = {
+  algo: algorithm,
+}
--- server/graphloader.js
+++ server/graphloader.js
@@ -4,9 +4,8 @@ var path = require('path');
 function loadGraph(number, callback) {
   var file = path.join(__dirname, './graphs/stellardata_'+number+'.json');
   fs.readFile(file, 'utf8', function(err, data) {
-    stars = JSON.parse(data).stars;
-    end = JSON.parse(data).endPoint;
-    callback(stars, end);
+    dataset = JSON.parse(data).dataset;
+    callback(dataset);
   });
 }
 
